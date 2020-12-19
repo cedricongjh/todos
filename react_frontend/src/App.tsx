7ea44bddb3c9cbb3938962474e2f17916857c44c
Cedric Ong <cedricong99@gmail.com>
2020-12-19 18:45:06 +0800

    started basic todo list

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [todos, setTodos] = useState([{id : 0, text: 'Learn Typescript'}, {id : 1, text: 'Cook Dinner'}])
+  const [input, setInput] = useState('')
+
+  const handleSubmit = (evt: any) => {
+    evt.preventDefault()
+    setTodos([...todos, {text: input, id: todos.length - 1}])
+    setInput('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Enter a todo here" value={input} onChange={e => setInput(e.target.value)}>
+      </input>
+      <button>ADD</button>
+    </form>
+  </div>
+  )
 }
 
 export default App;
