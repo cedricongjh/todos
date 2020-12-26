da922c5a822eabe74b92c6cdda8f6b81ac7dfbc2
Cedric Ong <cedricong99@gmail.com>
2020-12-26 20:47:49 +0800

    completed api for CRUD of todos

--- Rails_Api/app/controllers/todos_controller.rb
+++ Rails_Api/app/controllers/todos_controller.rb
@@ -2,8 +2,13 @@ class TodosController < ApplicationController
 
     def index
         todos = Todo.order('due ASC')
-        render json: {status: 'SUCCESS', message: 'fetched todos', data: todos}
+        render json: {status: 'SUCCESS', message: 'fetched todos', data: todos}, status: :ok
     end 
+
+    def show
+        todo = Todo.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded todo', data: todo}, status: :ok
+    end
     
     def create
         todo = Todo.new(todo_params)
@@ -12,6 +17,21 @@ class TodosController < ApplicationController
             render json: {status: 'SUCCESS', message: 'todo saved', data: todo}, status: :ok
         else
             render json: {status: 'ERROR', mesaage: 'Todo not saved', data: todo.errors}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        todo = Todo.find(params[:id])
+        todo.destroy
+        render json: {status: 'SUCCESS', message: 'todo deleted', data: todo}, status: :ok
+    end
+
+    def update
+        todo = Todo.find(params[:id])
+        if todo.update(todo_params)
+            render json: {status: 'SUCCESS', message: 'todo updated', data: todo}, status: :ok
+        else
+            render json: {status: 'ERROR', mesaage: 'Todo not updated', data: todo.errors}, status: :unprocessable_entity
         end
     end
 
