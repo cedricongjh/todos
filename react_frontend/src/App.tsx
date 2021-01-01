42a3ef47bd6b76a6b29312e19990e419aa6ed5fd
Cedric Ong <cedricong99@gmail.com>
2021-01-01 21:56:31 +0800

    added form for todo fields

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -2,22 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios  from 'axios'
 
-const App = () => {
+const App: React.FC = () => {
 
   const [todos, setTodos] = useState<Todo[]>([])
-  const [input, setInput] = useState('')
+  const [todo, setTodo] = useState<Todo>({text: '', id: todos.length + 1, category: '', due: '', completed: false})
 
   interface Todo {
     text: string
     id: number
+    category?: string
+    due?: string
+    completed: boolean
   }
 
   const handleSubmit = (evt: any) => {
     evt.preventDefault()
-    axios.post('/todos', {'text': input, 'completed': false, 'due': "2020-12-29", 'category': null}).then((resp: any) => {
+    axios.post('/todos', {'text': todo.text, 'completed': todo.completed, 'due': todo.due, 'category': todo.category}).then((resp: any) => {
       console.log(resp)
-      setTodos([...todos, {text: input, id: todos.length - 1}])
-      setInput('')
+      setTodos([...todos, todo])
+      setTodo({text: '', id: todos.length + 1, category: '', due: '', completed: false})
     })
   }
 
@@ -30,10 +33,13 @@ const App = () => {
 
   return (
   <div>
-    {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+    <div>TODO CATEGORY DUE</div>
+    {todos.map(todo => <div key={todo.id}>{todo.text} {todo.category} {todo.due}</div>)}
     <form onSubmit={handleSubmit}>
-      <input placeholder="Enter a todo here" value={input} onChange={e => setInput(e.target.value)}>
-      </input>
+      <input type="checkbox" checked={todo.completed} onChange={e => setTodo({...todo, completed: e.target.checked})}/>
+      <input placeholder="Enter a todo here" value={todo.text} onChange={e => setTodo({...todo, text: e.target.value})} />
+      <input placeholder="Enter a category here" value={todo.category} onChange={e => setTodo({...todo, category: e.target.value})} />
+      <input placeholder="Enter a due date here" value={todo.due} onChange={e => setTodo({...todo, due: e.target.value})} />
       <button>ADD</button>
     </form>
   </div>
