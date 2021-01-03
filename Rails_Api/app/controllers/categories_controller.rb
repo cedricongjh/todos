d42c7dddd9dd380442291b668826a86b005743e5
Cedric Ong <cedricong99@gmail.com>
2021-01-03 22:03:14 +0800

    added update and delete of categories

--- Rails_Api/app/controllers/categories_controller.rb
+++ Rails_Api/app/controllers/categories_controller.rb
@@ -5,14 +5,34 @@ class CategoriesController < ApplicationController
         render json: {status: 'SUCCESS', message: 'fetched categories', data: categories}, status: :ok
     end
 
-    def create
-        todo = Category.new(category_params)
+    def show
+        category = Category.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded category', data: category}, status: :ok
+    end
 
-        if todo.save
-            render json: {status: 'SUCCESS', message: 'category saved', data: todo}, status: :ok
+    def create
+        category = Category.new(category_params)
+
+        if category.save
+            render json: {status: 'SUCCESS', message: 'category saved', data: category}, status: :ok
         else
-            render json: {status: 'ERROR', mesaage: 'category not saved', data: todo.errors}, status: :unprocessable_entity
+            render json: {status: 'ERROR', mesaage: 'category not saved', data: category.errors}, status: :unprocessable_entity
         end
+    end
+
+    def update
+        category = Category.find(params[:id])
+        if category.update(category_params)
+            render json: {status: 'SUCCESS', message: 'category updated', data: category}, status: :ok
+        else
+            render json: {status: 'ERROR', mesaage: 'category not updated', data: category.errors}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        category = Category.find(params[:id])
+        category.destroy
+        render json: {status: 'SUCCESS', message: 'category deleted', data: category}, status: :ok
     end
 
     private
