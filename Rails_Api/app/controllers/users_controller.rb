8e109700cc8c7f08f6b59c2fd254e9476fde0493
Cedric Ong <cedricong99@gmail.com>
2021-01-15 23:47:42 +0800

    configured user controller

--- Rails_Api/app/controllers/users_controller.rb
+++ Rails_Api/app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+    before_action :authorised, only: [:auto_login]
+
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            token = encode_JWT({user_id: user.id})
+            render json: {status: 'SUCCESS', message: 'user signed up', data: @user, token: token}, status: :ok
+        else
+            render json: {status: 'ERROR', message: 'failed to sign up', data: @user.errors}, status: :unprocessable_entity
+        end
+    end
+
+    def login
+        
+        @user = User.find_by(email: params[:email])
+        
+        if @user && @user.authenticate(params[:password])
+            token = encode_JWT({user_id: @user.id})
+            render json: {status: 'SUCCESS', message: 'user logged in', data: @user, token: token}, status: :ok
+        else
+            render json: {status: 'ERROR', message: 'failed to log in', data: []}, status: :unprocessable_entity
+        end
+    end
+
+    def auto_login
+        render json: {status: 'SUCCESS', message: 'user logged in', data: @user}, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:email, :password)
+    end
+
 end
