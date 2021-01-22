e83c31e4e119affd8a99a048fabc0ab811e315bd
Cedric Ong <cedricong99@gmail.com>
2021-01-22 20:19:02 +0800

    created color column for category

--- Rails_Api/db/schema.rb
+++ Rails_Api/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_163211) do
+ActiveRecord::Schema.define(version: 2021_01_22_120902) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "color"
   end
 
   create_table "todos", force: :cascade do |t|
