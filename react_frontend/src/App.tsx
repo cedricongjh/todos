bde20ac57ceb5110a146557a17d23c06b9fc50f2
Cedric Ong <cedricong99@gmail.com>
2021-01-27 21:20:10 +0800

    configured production and dev env for FE

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -14,6 +14,9 @@ const App: React.FC = () => {
     }
     return Promise.reject(err)
   })
+
+  axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
+  axios.defaults.withCredentials = true
   
   useEffect(() => {
     setLoading(true)
